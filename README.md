9d07653569c9263fd5e89a1ecc07e3382874f623
章小慢 <manyangz@jumei.com>
2017-11-20 18:18:06 +0800

    update style

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
-# timetodate
-alfredworkflow timetodate base php
+# 时间转换.
 
-alfredworkflow php 试手小demo
+### date to timestamp
+
+```
+php main.php "2017-11-11 11:11:11";
+
+result;
+
+{"items":[{"arg":1510369871,"autocomplete":"2017-11-11 11:11:11","icon":{"path":"icon.png"},"subtitle":1510369871,"title":"timestamp","type":"default","uid":"2","valid":true}]}
+
+```
+
+### timestamp to date 
+
+```
+php main.php 1510369871
+
+result:
+
+{"items":[{"arg":"2017-11-11 11:11:11","autocomplete":"2017-11-11 11:11:11","icon":{"path":"icon.png"},"subtitle":"2017-11-11 11:11:11","title":"Y-m-d H:i:s","type":"default","uid":"1","valid":true}]}
+```
+
 
--- main.php
+++ main.php
@@ -5,3 +5,41 @@
  * @author 章小慢<manyangz@jumei.com>
  * @date 2017/11/20 11:09
  */
+require "vendor/autoload.php";
+
+use Alfred\Workflows\Workflow;
+
+$workflow = new Workflow;
+
+unset($argv[0]);
+$str = implode(" ", $argv);
+
+if(strval(strtotime(date('Y-m-d H:i:s',$str))) == $str) {
+    $result = date("Y-m-d H:i:s",$str);
+    $workflow->result()
+        ->uid('1')
+        ->title('Y-m-d H:i:s')
+        ->subtitle($result)
+        ->type('default')
+        ->arg($result)
+        ->valid(true)
+        ->icon('icon.png')
+        ->autocomplete($result);
+} else {
+    $result = strtotime($str);
+    $workflow->result()
+        ->uid('2')
+        ->title('timestamp')
+        ->subtitle($result)
+        ->type('default')
+        ->arg($result)
+        ->valid(true)
+        ->icon('icon.png')
+        ->autocomplete($str);
+}
+
+echo $workflow->output();
+
+?>
+
+
